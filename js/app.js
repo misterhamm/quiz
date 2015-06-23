86922d5171b6af1a3ff339dcd02813897ca37fe8
Chris Hamm <chris.m.hamm@gmail.com>
2015-06-22 21:03:39 -0400

    Quiz advancing and logging answers correctly

--- js/app.js
+++ js/app.js
@@ -13,135 +13,195 @@ $(document).ready(function(){
     var count = 0;
     var total = 5;
 
-    $('.answer-a1').click(function() {
+    //Question 1
+    $('.answer-a1').on('click', function() {
         finalAnswers.push('a');
+        count++;
         console.log(count);
         console.log(finalAnswers);
-        $('#question1' + count).css('display', 'none');
-        count++;
-        $('#question2' + count).css('display', 'block');
+        $('#question1').css('display', 'none');
+        $('#question2').css('display', 'block');
     });
 
+    $('.answer-b1').on('click', function() {
+        finalAnswers.push('b');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question1').css('display', 'none');
+        $('#question2').css('display', 'block');
+    });
 
+    $('.answer-c1').on('click', function() {
+        finalAnswers.push('c');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question1').css('display', 'none');
+        $('#question2').css('display', 'block');
+    });
 
+    $('.answer-d1').on('click', function() {
+        finalAnswers.push('d');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question1').css('display', 'none');
+        $('#question2').css('display', 'block');
+    });
+
+    //Question 2
+    $('.answer-a2').on('click', function() {
+        finalAnswers.push('a');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question2').css('display', 'none');
+        $('#question3').css('display', 'block');
+    });
+
+    $('.answer-b2').on('click', function() {
+        finalAnswers.push('b');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question2').css('display', 'none');
+        $('#question3').css('display', 'block');
+    });
+
+    $('.answer-c2').on('click', function() {
+        finalAnswers.push('c');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question2').css('display', 'none');
+        $('#question3').css('display', 'block');
+    });
+
+    $('.answer-d2').on('click', function() {
+        finalAnswers.push('d');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question2').css('display', 'none');
+        $('#question3').css('display', 'block');
+    });
+
+    //Question 3
+    $('.answer-a3').on('click', function() {
+        finalAnswers.push('a');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question3').css('display', 'none');
+        $('#question4').css('display', 'block');
+    });
+
+    $('.answer-b3').on('click', function() {
+        finalAnswers.push('b');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question3').css('display', 'none');
+        $('#question4').css('display', 'block');
+    });
+
+    $('.answer-c3').on('click', function() {
+        finalAnswers.push('c');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question3').css('display', 'none');
+        $('#question4').css('display', 'block');
+    });
+
+    $('.answer-d3').on('click', function() {
+        finalAnswers.push('d');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question3').css('display', 'none');
+        $('#question4').css('display', 'block');
+    });
+
+    //Question 4
+    $('.answer-a4').on('click', function() {
+        finalAnswers.push('a');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question4').css('display', 'none');
+        $('#question5').css('display', 'block');
+    });
+
+    $('.answer-b4').on('click', function() {
+        finalAnswers.push('b');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question4').css('display', 'none');
+        $('#question5').css('display', 'block');
+    });
+
+    $('.answer-c4').on('click', function() {
+        finalAnswers.push('c');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question4').css('display', 'none');
+        $('#question5').css('display', 'block');
+    });
+
+    $('.answer-d4').on('click', function() {
+        finalAnswers.push('d');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question4').css('display', 'none');
+        $('#question5').css('display', 'block');
+    });
+
+    //Question 5
+    $('.answer-a5').on('click', function() {
+        finalAnswers.push('a');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question5').css('display', 'none');
+        $('.data').css('display', 'block');
+    });
+
+    $('.answer-b5').on('click', function() {
+        finalAnswers.push('b');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question5').css('display', 'none');
+        $('.data').css('display', 'block');
+    });
+
+    $('.answer-c5').on('click', function() {
+        finalAnswers.push('c');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question5').css('display', 'none');
+        $('.data').css('display', 'block');
+    });
+
+    $('.answer-d5').on('click', function() {
+        finalAnswers.push('d');
+        count++;
+        console.log(count);
+        console.log(finalAnswers);
+        $('#question5').css('display', 'none');
+        $('.data').css('display', 'block');
+    });
 
 });
 
 
 
-
-//    // Quiz Answer Functionality
-//    var finalAnswers = [];
-//    var correctAnswers = ['b', 'c', 'd', 'a', 'a'];
-//    var total = 5;
-//    var count = 0;
-//
-//    function answerQuestion(answerA, answerB, answerC, answerD) {
-//        this.answerA = answerA;
-//        this.answerB = answerB;
-//        this.answerC = answerC;
-//        this.answerD = answerD;
-//        console.log(this);
-//    }
-//
-//    var question1 = function() {
-//        self = this;
-//        self.answerA = function(){
-//            finalAnswers.push('a');
-//            console.log(finalAnswers)
-//            count++
-//            console.log(count)
-//            document.getElementById('question1').style.display = 'none';
-//            document.getElementById('question2').style.display = 'block';
-//            return finalAnswers;
-//        }
-//        self.answerB = function(){
-//            finalAnswers.push('b');
-//            console.log(finalAnswers)
-//            count++
-//            console.log(count)
-//            document.getElementById('question1').style.display = 'none';
-//            document.getElementById('question2').style.display = 'block';
-//            return finalAnswers;
-//        }
-//        self.answerC = function(){
-//            finalAnswers.push('c');
-//            console.log(finalAnswers)
-//            count++
-//            console.log(count)
-//            document.getElementById('question1').style.display = 'none';
-//            document.getElementById('question2').style.display = 'block';
-//            return finalAnswers;
-//        }
-//        self.answerD = function(){
-//            finalAnswers.push('d');
-//            console.log(finalAnswers)
-//            count++
-//            console.log(count)
-//            document.getElementById('question1').style.display = 'none';
-//            document.getElementById('question2').style.display = 'block';
-//            return finalAnswers;
-//        }
-//        return self;
-//    }();
-//
-//    var question2 = function() {
-//        self = this;
-//        self.answerA = function(){
-//            finalAnswers.push('a');
-//            console.log(finalAnswers)
-//            count++
-//            console.log(count)
-//            document.getElementById('question2').style.display = 'none';
-//            document.getElementById('question3').style.display = 'block';
-//            return finalAnswers;
-//        }
-//        self.answerB = function(){
-//            finalAnswers.push('b');
-//            console.log(finalAnswers)
-//            count++
-//            console.log(count)
-//            document.getElementById('question2').style.display = 'none';
-//            document.getElementById('question3').style.display = 'block';
-//            return finalAnswers;
-//        }
-//        self.answerC = function(){
-//            finalAnswers.push('c');
-//            console.log(finalAnswers)
-//            count++
-//            console.log(count)
-//            document.getElementById('question2').style.display = 'none';
-//            document.getElementById('question3').style.display = 'block';
-//            return finalAnswers;
-//        }
-//        self.answerD = function(){
-//            finalAnswers.push('d');
-//            console.log(finalAnswers)
-//            count++
-//            console.log(count)
-//            document.getElementById('question2').style.display = 'none';
-//            document.getElementById('question3').style.display = 'block';
-//            return finalAnswers;
-//        }
-//        return self;
-//    }();
-//
-////console.log(question1.);
-//
-//    var q = question1;
-//
-//    document.getElementById('answer-a').onclick=q.answerA
-//    document.getElementById('answer-b').onclick=q.answerB
-//    document.getElementById('answer-c').onclick=q.answerC
-//    document.getElementById('answer-d').onclick=q.answerD
-//
-//
-//
-//
-//
-//
-////   document.getElementsByClass('')[0].onclick=answerA
-//
 //   /*grade = function(){
 //     if(finalAnswers[0] != correctAnswers[0]){
 //       total--
